191b4dfa15c903b35cae05b96d2c7d7db0841f9f
saifulislam05 <saiful.bijulia@gmail.com>
2022-09-20 05:54:36 +0530

    Essential Product done..

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
+import EssentialProducts from "../app/components/Home/EssentialProducts";
 import FeaturedBrands from "../app/components/Home/FeaturedBrands";
+import HealthDaily from "../app/components/Home/HealthDaily";
 import HomeCategory from "../app/components/Home/HomeCategory";
 import HomeHero from "../app/components/Home/HomeHero";
 import HomePoster from "../app/components/Home/HomePoster";
 import TodaysDeals from "../app/components/Home/TodaysDeals";
-import Products from "./products";
 
+import { setBackgroundImage } from '../app/utils/helpers'
 
 export default function Home() {
   return (
@@ -14,6 +16,14 @@ export default function Home() {
       <FeaturedBrands />
       <TodaysDeals />
       <HomePoster />
+
+      <div className="py-5" style={setBackgroundImage('https://demo4.drfuri.com/farmart2/wp-content/uploads/sites/11/2021/05/homepage-new-content-bg-scaled.jpg', { backgroundPosition: 'bottom center', backgroundColor: '#f3f3f3', backgroundSize: 'cover' })}>
+        <EssentialProducts />
+        <HealthDaily />
+      </div>
+
+
+
     </>
   )
 }
