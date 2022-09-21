931a77a3f81e4b115e8ba96c431863762da0ea34
saifulislam05 <saiful.bijulia@gmail.com>
2022-09-21 18:37:05 +0530

    Product Info done..

--- app/components/Product/Additional.js
+++ app/components/Product/Additional.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Additional = () => {
   return (
-    <div>Additional</div>
+      <div>
+          <h1 className="text-title md:text-xl text-lg font-bold mb-5">Additional</h1>
+          <div className="flex border">
+              <div className="w-1/2 p-2 px-4 text-sm bg-gray-100">
+                  Weight
+              </div>
+              
+              <div className="w-1/2 p-2 px-4 text-sm">
+                  500g
+              </div>
+             
+          </div>
+    </div>
   )
 }
 
--- app/components/Product/Reviews.js
+++ app/components/Product/Reviews.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Reviews = () => {
   return (
-    <div>Reviews</div>
+      <div className='border p-5'>
+          <h1 className="text-orange font-bold md:text-6xl text-3sxl ">5.00</h1>
+          <p className="text-sm">⭐⭐⭐⭐⭐</p>
+          <p className="text-sm">Based on 1 Reviews</p>
+    </div>
   )
 }
 
