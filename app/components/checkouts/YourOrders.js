f84404b28cb6f54f1ebf1279744a19b79c639401
saifulislam05 <saiful.bijulia@gmail.com>
2022-10-16 16:34:04 +0530

    checkout Your Orders

--- app/components/checkouts/YourOrders.js
+++ app/components/checkouts/YourOrders.js
@@ -1,39 +1,68 @@
-import React from 'react'
+import Button  from "../shared/Button";
+import React from "react";
 
 const YourOrders = () => {
   return (
     <div>
       <h3 className="text-2xl mb-4 ">Your Orders</h3>
-      <div className="bg-gray-100  p-5 rounded-md">
+      <div className="bg-gray-50  p-5 rounded-md">
         <div className="flex justify-between border-b">
           <h2 className="text-lg font-bold">PRODUCT</h2>
           <h2 className="text-lg font-bold">TOTAL</h2>
         </div>
-        <div className="py-5 border-b space-y-3 bg-white">
-
-          <div className="flex justify-between border-b py-5">
-            <h2 className="text-base font-medium">Soft Mochi & Galeto Ice Cream <span className="text-gray-400">(x2)</span></h2>
+        <div className="space-y-3 bg-white">
+          <div className="flex justify-between border-b py-5 px-5">
+            <h2 className="text-base font-medium">
+              Soft Mochi & Galeto Ice Cream{" "}
+              <span className="text-gray-400">(x2)</span>
+            </h2>
             <h2 className="text-base font-semibold ml-2">$41.39</h2>
           </div>
-
-          <div className="flex justify-between border-b py-5">
-            <h2 className="text-base font-medium">Soft Mochi & Galeto Ice Cream <span className="text-gray-400">(x1)</span></h2>
+          <div className="flex justify-between border-b py-5 px-5">
+            <h2 className="text-base font-medium">
+              Soft Mochi & Galeto Ice Cream{" "}
+              <span className="text-gray-400">(x2)</span>
+            </h2>
             <h2 className="text-base font-semibold ml-2">$41.39</h2>
           </div>
-
-          <div className="flex justify-between border-b py-5">
-            <h2 className="text-base font-medium">Soft Mochi & Galeto Ice Cream <span className="text-gray-400">(x1)</span></h2>
+          <div className="flex justify-between border-b py-5 px-5">
+            <h2 className="text-base font-medium">
+              Soft Mochi & Galeto Ice Cream{" "}
+              <span className="text-gray-400">(x2)</span>
+            </h2>
             <h2 className="text-base font-semibold ml-2">$41.39</h2>
           </div>
-
         </div>
-        <div className="flex justify-between py-5">
-          <h2 className="text-xl font-bold">Subtotal</h2>
-          <h2 className="text-xl font-bold text-green">$200.00</h2>
+
+          {/* Total amount as per product  */}
+
+        <div className="flex justify-between py-5 border-b">
+          <h2 className="text-lg font-bold">Subtotal</h2>
+          <h2 className="text-lg font-bold text-green">$200.00</h2>
+        </div>
+
+        <div className="py-5 border-b space-y-3">
+          <div className="text-title">Shipping</div>
+          <ul className="">
+            <li className="">- Flat rate</li>
+          </ul>
+        </div>
+        <div className="flex justify-between py-5 border-b">
+          <h2 className="text-lg font-bold">Total</h2>
+          <h2 className="text-lg font-bold text-green">$200</h2>
+        </div>
+        <div className="pt-5">
+          <h2 className="text-lg font-bold">Payment method</h2>
+          <p className="text-gray-500 mt-3"> - Pay with a Credit/Debit Card</p>
         </div>
       </div>
+      <Button
+        title="Place Order"
+      className="w-full py-3 mt-5"
+      />
+
     </div>
   );
-}
+};
 
-export default YourOrders
+export default YourOrders;
--- app/components/shared/Button.js
+++ app/components/shared/Button.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import { Loader } from "@mantine/core"
 
-const Button = ({ className, title, ...rest }) => {
+
+const Button = ({ className, title, loading, ...rest }) => {
     return (
-        <button className={`bg-primary font-bold px-6 rounded ${className} `} {...rest}>{title}</button>
+        <button className={`bg-primary font-bold px-6 rounded ${className} `} {...rest}>
+            {loading ? (<Loader color="gray" size="sm" className="mx-auto"/>): title}
+        
+        </button>
     )
 }
 
